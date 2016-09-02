9b2694efa681fb3e5fc42986a030b15dea5229ce
ermishechkin <erman15@yandex.ru>
2016-09-02 20:39:05 +0300

    разработали форму и добавили логгирование запросов

---
+++ public/main.js
@@ -0,0 +1,13 @@
+'use strict';
+function onSubmit(form) {
+  let data = {
+    user: form.elements['user'].value,
+    email: form.elements['email'].value
+  };
+
+  let result = request('/users', data);
+  if (result === '100') {
+    form.hidden = true;
+  }
+  console.log(data);
+}
